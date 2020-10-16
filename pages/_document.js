8dae13994f033f8401bf6ce4e0f0043081574f90
jai-sharma <sendtojsharma@gmail.com>
2020-10-16 14:56:20 +0530

    added meta tag for Google Console

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta
+                        name="google-site-verification"
+                        content="jTfbO07fKb3IRFwa3Rqy6nWxZV7BtINaQC1iX9uDgxc"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
