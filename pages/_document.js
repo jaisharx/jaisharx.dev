b6c24a4678e1fcdaf9a1dc80273e7febc64b1b9c
jai-sharma <sendtojsharma@gmail.com>
2020-10-21 08:56:15 +0530

    added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,10 +10,31 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <meta
-                        name="google-site-verification"
-                        content="jTfbO07fKb3IRFwa3Rqy6nWxZV7BtINaQC1iX9uDgxc"
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/favicon/apple-touch-icon.png"
                     />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/favicon/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/favicon/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="/favicon/site.webmanifest" />
+                    <link
+                        rel="mask-icon"
+                        href="/favicon/safari-pinned-tab.svg"
+                        color="#5bbad5"
+                    />
+                    <meta name="msapplication-TileColor" content="#da532c" />
+                    <meta name="theme-color" content="#ffffff"></meta>
                 </Head>
                 <body>
                     <Main />
