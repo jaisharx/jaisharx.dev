c461f99ac393ab35c9d59c4bd45afc8058a46443
jai-sharma <sendtojsharma@gmail.com>
2020-10-22 11:38:00 +0530

    added contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,54 @@
 import styles from '../styles/contact.module.scss';
-import Link from 'next/link';
+import Navbar from '../components/navbar';
 
 function contact() {
     return (
-        <main className={styles.container}>
-            {/* <button className={styles.close}>
-                <Link href="">X</Link>
-            </button> */}
-        </main>
+        <>
+            <Navbar active="contact" isBack="true" />
+            <div className={styles.container}>
+                <ul>
+                    <li>
+                        <a
+                            href="https://twitter.com/jaisharx"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            01. Twitter
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://github.com/jaisharx"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            02. Github
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://dev.to/jaisharx"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            03. Dev.to
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://medium.com/@jaisharx"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            04. Medium
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">05. Good ol' Mail</a>
+                    </li>
+                </ul>
+            </div>
+        </>
     );
 }
 
