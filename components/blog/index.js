224b617f612dd63b90f24388e40847a83b70bbbd
jai-sharma <sendtojsharma@gmail.com>
2020-10-24 12:52:13 +0530

    added markdown support

--- components/blog/index.js
+++ components/blog/index.js
@@ -1,9 +1,11 @@
 import styles from './blog.module.scss';
+import MarkdownView from 'react-showdown';
 
 function index({ content }) {
     return (
         <main className={styles.container}>
             <h1>Blogs</h1>
+            <MarkdownView markdown={content} options={{ tables: true, emoji: true }} />
             <p dangerouslySetInnerHTML={{ __html: content }}></p>
         </main>
     );
