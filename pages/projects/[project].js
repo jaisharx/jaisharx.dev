eebc092ef2416e82478ae20a90996ec43231ba5f
jai-sharma <sendtojsharma@gmail.com>
2020-10-24 19:40:32 +0530

    added progress bar and signature

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -1,17 +1,22 @@
 import styles from '../../styles/eachpage.module.scss';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
+import Signature from '../../components/signature';
 import PageData from '../../components/page-data';
+import ProgressWrapper from '../../components/progress';
 
 function project({ project }) {
     return (
-        <>
+        <ProgressWrapper>
             <header>
                 <Navbar isBack="true" active="projects" />
             </header>
             <PageData data={project} />
+            <p className={styles.signatureContainer}>
+                <Signature />
+            </p>
             <Footer />
-        </>
+        </ProgressWrapper>
     );
 }
 
