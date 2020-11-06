5b686ddcc5e4367f4605a64f9036d6f7582d532f
jai-sharma <sendtojsharma@gmail.com>
2020-11-06 09:07:49 +0530

    fixed seo

---
+++ components/helmet/siteConfig.js
@@ -0,0 +1,26 @@
+export default {
+    author: 'Jai Sharma',
+    title: 'jaisharma.dev | my all-in-one platform for publishing content',
+    name: 'jaisharma.dev',
+    description:
+        "Hi, there! Nice to have you around. You're on my personal website right now. I built this as an one-spot online collection for all my thoughts and projects. Like a diary that I could maintain for myself and others like me interested in the tech, philosophy, or future.",
+    twitter: 'jaisharx',
+    facebook: 'jaisharx',
+    logo: '/logo.png',
+    logoSquare: '/logo.png',
+    url: {
+        web: 'https://jaisharma.dev/',
+        repo: 'https://github.com/jaisharx/jaisharx.dev',
+    },
+    keywords: [
+        'Jai Sharma',
+        'jaisharx',
+        'Web Developer',
+        'frontend developer',
+        'backend developer',
+        'frontend',
+        'backend',
+        'devops',
+        'build with next and strapi',
+    ],
+};
