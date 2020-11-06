cfebf836e6bec409a7ecf9ecc6a8a22c48b24238
jai-sharma <sendtojsharma@gmail.com>
2020-11-06 06:41:59 +0530

    testing some options for the img component

--- components/title/index.js
+++ components/title/index.js
@@ -19,19 +19,47 @@ function Title() {
             <TextLoop interval="2200">
                 <div className={styles.text}>
                     <p>I use Javascript</p>
-                    <Image width="30" height="30" alt="Icon" src="/icons/javascript.svg" />
+                    <Image
+                        width="30"
+                        height="30"
+                        alt="Icon"
+                        loading="eager"
+                        priority="true"
+                        src="/icons/javascript.svg"
+                    />
                 </div>
                 <div className={styles.text}>
                     <p>I use Typescript</p>
-                    <Image width="30" height="30" alt="Icon" src="/icons/typescript.svg" />
+                    <Image
+                        width="30"
+                        height="30"
+                        alt="Icon"
+                        loading="eager"
+                        priority="true"
+                        src="/icons/typescript.svg"
+                    />
                 </div>
                 <div className={styles.text}>
                     <p>I use GraphQL</p>
-                    <Image width="30" height="30" alt="Icon" src="/icons/graphql.svg" />
+                    <Image
+                        width="30"
+                        height="30"
+                        alt="Icon"
+                        loading="eager"
+                        priority="true"
+                        src="/icons/graphql.svg"
+                    />
                 </div>
                 <div className={styles.text}>
                     <p>I use Docker</p>
-                    <Image width="30" height="30" alt="Icon" src="/icons/docker.svg" />
+                    <Image
+                        width="30"
+                        height="30"
+                        alt="Icon"
+                        loading="eager"
+                        priority="true"
+                        src="/icons/docker.svg"
+                    />
                 </div>
             </TextLoop>
         </div>
