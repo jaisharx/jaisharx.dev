3b75ad98d80444267e29bda0334b8b322f57e1a0
jai-sharma <sendtojsharma@gmail.com>
2020-11-06 07:49:05 +0530

    using svgs inline

--- components/title/index.js
+++ components/title/index.js
@@ -2,27 +2,36 @@ import React from 'react';
 import TextLoop from 'react-text-loop';
 import styles from './style.module.scss';
 
+import Javascript from './icons/javascript.svg';
+import Typescript from './icons/typescript.svg';
+import Git from './icons/git.svg';
+import Node from './icons/node.svg';
+import Reactjs from './icons/reactjs.svg';
+
+const Icons = [Javascript, Typescript, Git, Node, Reactjs];
+
 function Title() {
+    function createList() {
+        const list = [];
+        Icons.forEach((Icon, index) => {
+            list.push(
+                <div className={styles.text} key={index}>
+                    <p>I use {Icon.name.slice(3)}</p>
+                    <Icon />
+                </div>
+            );
+        });
+
+        return list;
+    }
+
     return (
         <div className={styles.container}>
-            <TextLoop interval="2200">
-                <div className={styles.text}>
-                    <p>I use Javascript</p>
-                    <img width="30" height="30" alt="Icon" src="/icons/javascript.svg" />
-                </div>
-                <div className={styles.text}>
-                    <p>I use Typescript</p>
-                    <img width="30" height="30" alt="Icon" src="/icons/typescript.svg" />
-                </div>
-                <div className={styles.text}>
-                    <p>I use GraphQL</p>
-                    <img width="30" height="30" alt="Icon" src="/icons/graphql.svg" />
-                </div>
-                <div className={styles.text}>
-                    <p>I use Docker</p>
-                    <img width="30" height="30" alt="Icon" src="/icons/docker.svg" />
-                </div>
-            </TextLoop>
+            <TextLoop
+                interval="2200"
+                children={createList()}
+                springConfig={{ stiffness: 280, damping: 18 }}
+            />
         </div>
     );
 }
