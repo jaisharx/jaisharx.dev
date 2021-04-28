6ccd4c1e856b4b83e6becfd61c6d3257838ed45b
jai-sharma <sendtojsharma@gmail.com>
2021-04-28 20:26:37 +0530

    added header section

--- components/header.js
+++ components/header.js
@@ -1,5 +1,23 @@
-import { Text, Box, Grid, HStack } from '@chakra-ui/react';
+import { Text, Box, Grid, HStack, Link } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import NextImage from 'next/image';
 import Container from './container';
+
+import GmailIcon from './icons/gmail-icon.svg';
+import GithubIcon from './icons/github-icon.svg';
+import DevToIcon from './icons/dev.to-icon.svg';
+import MediumIcon from './icons/medium-icon.svg';
+import DribbleIcon from './icons/dribble-icon.svg';
+
+const SocialLink = styled(Link)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 64px;
+    height: 64px;
+    border: 1px solid white;
+    border-radius: 100%;
+`;
 
 function LeftColumn() {
     return (
@@ -13,11 +31,25 @@ function LeftColumn() {
             </Text>
             <Text fontSize="large" mt={6}>
                 I’m a Software Developer from New Delhi, India. I specialize in
-                creating high quality <br /> websites & webapps made with love
-                of modern web technologies.
+                creating high quality <br /> <u>websites</u> & <u>webapps</u>{' '}
+                made with love of modern web technologies.
             </Text>
-            <HStack mt={10}>
-                
+            <HStack spacing={9} mt={10}>
+                <SocialLink>
+                    <GmailIcon />
+                </SocialLink>
+                <SocialLink>
+                    <GithubIcon />
+                </SocialLink>
+                <SocialLink>
+                    <DevToIcon />
+                </SocialLink>
+                <SocialLink>
+                    <MediumIcon />
+                </SocialLink>
+                <SocialLink>
+                    <DribbleIcon />
+                </SocialLink>
             </HStack>
         </Box>
     );
@@ -29,6 +61,7 @@ export default function Header() {
             <Container>
                 <Grid templateColumns="1.4fr 1fr">
                     <LeftColumn />
+                    <NextImage src="/profile.jpg" width="1246" height="1664" />
                 </Grid>
             </Container>
         </Box>
