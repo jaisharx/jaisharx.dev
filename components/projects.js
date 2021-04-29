c189cb7053b472770105c00d0dfd3a4cf70b0e8b
jai-sharma <sendtojsharma@gmail.com>
2021-04-29 11:33:03 +0530

    fixing formatting

--- components/projects.js
+++ components/projects.js
@@ -71,10 +71,9 @@ export default function Projects() {
                     url="https://dev-to-pied.vercel.app/"
                     githubUrl="https://github.com/jaisharx/dev.to"
                 >
-                    Dev.to clone built by me from scratch (yeah, no YT tuts).{' '}
-                    <br />
-                    It's using react & nextjs as its framework, and chakra UI
-                    for themeability.
+                    Dev.to clone built by me from scratch (yeah, no YT tuts). <br />
+                    It's using react & nextjs as its framework, and chakra UI for
+                    themeability.
                 </Project>
                 <Project
                     count="02"
@@ -83,10 +82,8 @@ export default function Projects() {
                     url="https://nextjs-org-website.vercel.app/"
                     githubUrl="https://github.com/jaisharx/nextjs.org"
                 >
-                    Nextjs homepage website has a very clean & modern look to
-                    it.
-                    <br /> That's why I rebuilt it pixel perfect to see how
-                    close I can get.
+                    Nextjs homepage website has a very clean & modern look to it.
+                    <br /> That's why I rebuilt it pixel perfect to see how close I can get.
                 </Project>
                 <Project
                     count="03"
@@ -95,7 +92,7 @@ export default function Projects() {
                     url="https://designwiz.vercel.app/"
                     githubUrl="https://github.com/jaisharx/designwiz"
                 >
-                    I rebuilt a modern looking design based website. <br/>
+                    I rebuilt a modern looking design based website. <br />
                     Achieving a score of 100 on a lighthouse audit as well.
                 </Project>
             </Container>
