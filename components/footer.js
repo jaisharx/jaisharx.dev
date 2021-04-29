4dc08551a9a259f10408a2c4be9074d141ef4478
jai-sharma <sendtojsharma@gmail.com>
2021-04-29 21:56:39 +0530

    fixing size

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <Box as="footer" id="contact" my={{ base: 20, md: 80 }}>
             <Container>
                 <Heading
-                    fontSize={{ base: '32px', md: '62px', lg: '92px' }}
+                    fontSize={{ base: '24px', md: '62px', lg: '92px' }}
                     color="white"
                     textShadow=" 1px 1px 0px #161919,
                     2px 2px 0px #161919,
