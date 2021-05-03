35dc8004525cfcd28d29fb7c2cb82ed82b75d3c5
jai-sharma <sendtojsharma@gmail.com>
2021-05-03 21:49:16 +0530

    moving to lib

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import GoogleAnalytics from 'components/google-analytics';
+import Analytics from 'lib/analytics';
 
 export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
                 <Head>
-                    <GoogleAnalytics />
+                    <Analytics />
                 </Head>
                 <body>
                     <Main />
